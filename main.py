c31025c3598c965ce759a7d50806afe04b851865
LittleJake <13583702+LittleJake@users.noreply.github.com>
2024-04-29 23:43:15 +0800

    Update

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def help_message():
     print("")
     print("--sS <IP>                TCP SYN Scan")
     print("--sn <IP>                ICMP Ping")
+    print("--sN/sF/sX <IP>          TCP Null, FIN, and Xmas scans")
     print("-p, --port <port>        Specific port. (Default: 80)")
     print("-c, --count <count>      Specific packet number. (Default: 1)")
     print("-6, --ipv6               Enable IPv6.")
@@ -32,7 +33,7 @@ def help_message():
 def main(argv):
     global COUNT, PORT, PROTOCOL
 
-    opts, _ = getopt.getopt(argv, "hp:c:6", ["help", "sn=", "sN=", "sS=", "sU=", "port=", "count=", "ipv6"])
+    opts, _ = getopt.getopt(argv, "hp:c:6", ["help", "sn=", "sN=", "sX=", "sS=","sF=", "sU=", "sA=", "port=", "count=", "ipv6"])
 
     for opt, arg in opts:
         if opt in ("-p", "--port"):
@@ -50,10 +51,19 @@ def main(argv):
             ping(server_address)
         if opt == "--sS":
             server_address = arg
-            syn_scan(server_address, PORT)
+            tcp_syn_scan(server_address, PORT)
         if opt == "--sN":
             server_address = arg
-            null_scan(server_address, PORT)
+            tcp_null_scan(server_address, PORT)
+        if opt == "--sA":
+            server_address = arg
+            tcp_ack_scan(server_address, PORT)
+        if opt == "--sF":
+            server_address = arg
+            tcp_fin_scan(server_address, PORT)
+        if opt == "--sX":
+            server_address = arg
+            tcp_xmas_scan(server_address, PORT)
         if opt == "--sU":
             server_address = arg
             udp_ping(server_address)
@@ -87,7 +97,7 @@ def ping(address):
         print("No response after 5s. Dead!")
 
 
-def syn_scan(address, port):
+def tcp_syn_scan(address, port):
     print("Sending TCP SYN request", address)
 
     result, _ = send_tcp_pkt(address, port, "S")
@@ -100,10 +110,51 @@ def syn_scan(address, port):
     else:
         print("No response after 5s. Closed!")
 
-def null_scan(address, port):
+
+def tcp_ack_scan(address, port):
+    print("Sending TCP SYN request", address)
+
+    result, _ = send_tcp_pkt(address, port, "A")
+
+    if len(result) > 0:
+        if result[0][1][1].flags in ("RA", "R"):
+            print("RST! Closed or Out-Of-Sync!")
+        else:
+            print("Filtered?")
+    else:
+        print("Filtered?")
+
+
+def tcp_null_scan(address, port):
     print("Sending TCP NULL request", address)
 
     result, _ = send_tcp_pkt(address, port, "")
+    if len(result) > 0:
+        if result[0][1][1].flags in ("RA", "R"):
+            print("RST! Closed!")
+        else:
+            print("No response! Open!")
+    else:
+        print("No response! Open!")
+
+
+def tcp_fin_scan(address, port):
+    print("Sending TCP NULL request", address)
+    
+    result, _ = send_tcp_pkt(address, port, "F")
+    if len(result) > 0:
+        if result[0][1][1].flags in ("RA", "R"):
+            print("RST! Closed!")
+        else:
+            print("No response! Open!")
+    else:
+        print("No response! Open!")
+
+
+def tcp_xmas_scan(address, port):
+    print("Sending TCP NULL request", address)
+    
+    result, _ = send_tcp_pkt(address, port, "FPU")
     if len(result) > 0:
         if result[0][1][1].flags in ("RA", "R"):
             print("RST! Closed!")
@@ -119,7 +170,9 @@ def send_tcp_pkt(address, port, flags):
     elif PROTOCOL == 6:
         pkt = IPv6(dst=address) / TCP(dport=port, flags=flags)
         
-    
+    if DEBUG:
+        print(pkt)
+
     return sr(pkt * COUNT, timeout=5, verbose=DEBUG)
 
 
